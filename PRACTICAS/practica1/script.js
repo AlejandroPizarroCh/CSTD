76a4e4d9b8be64ffe5a6605611828a26364e567d
Alejandro Pizarro <A01633784@tec.mx>
2022-02-22 17:26:18 -0600

    practica 1 HTML

---
+++ PRACTICAS/practica1/script.js
@@ -0,0 +1,22 @@
+//aqui dentro va javascript
+//ojo - cambio de comentarios
+
+//declarar variables
+x = 5;
+x = "esto se puede";
+console.log("Hola desde afuera");
+function ejemplo() {
+    console.log("Hola a todxs!");
+    //alert("Hola a todxs!"); --> alertas de advertencia 
+}
+function boton2() {
+    alert("Boton dos");
+}
+function boton3() {
+    alert("Boton tres");
+    //el documento se encuentra jerarquizado en una estructura que se llama
+    //DOM  - Document Object Model
+
+    //podemos acceder a un elemento especifico por medio de id
+    document.getElementById("parrafito").innerHTML = "<h1>cambie para siempre...</h1>";
+}
