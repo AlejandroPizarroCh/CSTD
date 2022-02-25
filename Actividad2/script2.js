64a4828101141c85580c04d4b7b814bfeaa409f7
Alejandro Pizarro <A01633784@itesm.mx>
2022-02-25 14:16:41 -0600

    actividad 2 - pagina retro FINAL

--- Actividad2/script2.js
+++ Actividad2/script2.js
@@ -1,7 +1,14 @@
+// Funnción de javascript que muestra mensaje de los campos de la forma y algo de texto extra 
+function muestraMensaje(){
+    d1 = document.getElementById("dato1").value;
+    d2 = document.getElementById("dato2").value;
+    d3 = document.getElementById("dato3").value;
 
-<script src = "mensaje.html"> </script>
+    t1 = "Es cierto, el "; 
+    t2 = " tiene cara de "; 
+    t3 = " pero parece "; 
 
-function botonMensaje(){
-    alert("Vas a salir de está página");
-
+//  concatenar datos 
+    document.getElementById("mensajito").innerHTML = t1 + d1 + t2 + d2 + t3 + d3; 
+   
 }
