fab4e8f174c52f3eaf7e6b135a98bcaab6ebeee6
Alejandro Pizarro <A01633784@itesm.mx>
2022-04-01 08:37:26 -0600

    actualizacion repo

--- react-node-app/server/index.js
+++ react-node-app/server/index.js
@@ -3,6 +3,79 @@
 const express = require("express"); 
 const PORT = process.env.PORT || 3001; 
 const app = express(); 
+const fs = require("fs"); 
+
+
+const newPet = {
+    "pet4" : {
+        "name" : "nala",
+        "type" : "dog",
+        "owner" : "eldelcesar",
+        "color" : "salt and peper",
+        "id": 4
+    }
+}
+
+const newPet2 = {
+    "pet5" : {
+        "name" : "xerath",
+        "type" : "inconclusive",
+        "owner" : "alejandrop",
+        "color": "gray and blue",
+        "id" : 5
+    }
+}
+
+
+app.get("/api", (req, res) => {
+    res.json({message: "Hello from server!"}); 
+}); 
+
+app.get("/pets", (req, res) => {
+    fs.readFile( __dirname + "/" + "pets.json", "utf8", (err, data) => {
+        console.log( data ); 
+        res.end( data ); 
+    }); 
+}); 
+
+app.post("/addPet", (req, res) => {
+    fs.readFile(__dirname + "/" + "pets.json", "utf8", (err, data) => {
+        data = JSON.parse( data );
+        data["pet4"] = newPet["pet4"];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+});
+
+app.put("/putPet", (req, res) => {
+    fs.readFile(__dirname + "/" + "pets.json", "utf8", (err, data) => {
+        data = JSON.parse(data); 
+        data["pet5"] = newPet2["pet5"]; 
+        console.log(data); 
+        res.end(JSON.stringify(data)); 
+    }); 
+}); 
+
+app.delete("/deletePet", function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "pets.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       delete data["pet4"];
+        
+       console.log( data );
+       res.end( JSON.stringify(data));
+    });
+ })
+
+//change JSON
+app.post('/addPet2', function (req, res) {
+
+    fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+        if (err) return console.log(err);
+        console.log(JSON.stringify(file));
+        console.log('writing to ' + fileName);
+      });
+ })
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`); 
